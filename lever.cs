14139efd243d9c02f71f146080d3565fdcee172f
Jacob Cunningham <jacob.joc@gmail.com>
2018-12-03 12:11:45 -0800

    Add files via upload

---
+++ lever.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lever : MonoBehaviour {
+
+    public GameObject maze;
+    public GameObject player;
+    public string levername;
+    private bool onoff;
+    public Vector2 position;
+    public float angle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(onoff){
+            if(Input.GetKeyDown(KeyCode.E)){
+                Debug.Log("pressed");
+                changemaze(levername);
+            }
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision start");
+        if(collision.gameObject.tag == "player"){
+            onoff = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("collision end");
+        if (collision.gameObject.tag == "player")
+        {
+            onoff = false;
+        } 
+    }
+
+
+    void changemaze(string m){
+        switch(m){
+            case "Maze 1 Level 1":
+                int i = maze.transform.childCount;
+                for (int l = 0; l < i; l++)
+                {
+                    maze.transform.GetChild(l).gameObject.transform.RotateAround(position, Vector3.forward, angle);
+                }
+                break;
+        }
+    }
+}
---
+++ player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+
+    public float speed;
+
+
+
+    void Start()
+    {
+
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+           
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        }
+
+       
+
+
+    }
+}
+
+
+
+
