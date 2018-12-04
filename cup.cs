91ef6afeb9afa56a5cccf904c531e9d3e6ba6e01
Jacob Cunningham <jacob.joc@gmail.com>
2018-12-04 12:29:01 -0800

    Add files via upload

---
+++ cup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cup : MonoBehaviour
+{
+
+
+    public List<string> cupsname;
+    public List<int> cupsvol;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < cupsname.Count; i++){
+           cup test  = new cup(cupsname[i], cupsvol[i]);
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public string Name { get; set; }
+    public int Volume { get; set; }
+    public cup() { }
+    public cup(string name, int age)
+    {
+        Name = name;
+        Volume = age;
+    }
+
+    void getcup(string cupname)
+    {
+        
+    }
+}
+
+
