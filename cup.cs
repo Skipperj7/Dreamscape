e1621567717543dd1e516b4e4f8e46c282ab1ac2
Jacob Cunningham <jacob.joc@gmail.com>
2018-12-06 12:19:50 -0800

    Add files via upload

--- cup.cs
+++ cup.cs
@@ -1,45 +1,121 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using System;
 public class cup : MonoBehaviour
 {
 
 
-    public List<string> cupsname;
-    public List<int> cupsvol;
+    public cupcreate script;
 
 
+    public int volume;
+    private int currentvol;
+    public string Name;
 
+    public Button thiscup;
+
+    public GameObject thisoptionscanvas;
+    public GameObject game;
+    public Button thisfill;
+    public Button thispour;
+    public Button thisempty;
 
     // Use this for initialization
     void Start()
     {
-        for (int i = 0; i < cupsname.Count; i++){
-        //need to create new var for each cup
-           cup test  = new cup(cupsname[i], cupsvol[i]);
-        } 
+        thiscup.onClick.AddListener(select);
+        thisoptionscanvas.SetActive(false);
+        try
+        {
+            script.getcupandsetvol("testcup", 3);
+        }
+        catch(Exception){
+            Debug.Log("error");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        foreach (cupcreate t in script.cupslist)
+        {
+            if (t.Name == Name)
+            {
+
+                currentvol = t.CurrentVolume;
+            }
+        }
+        thiscup.transform.GetComponentInChildren<Text>().text = currentvol.ToString();
 
     }
 
-    public string Name { get; set; }
-    public int Volume { get; set; }
-    public cup() { }
-    public cup(string name, int age)
+    void select()
     {
-        Name = name;
-        Volume = age;
+        thisoptionscanvas.SetActive(true);
     }
 
-    void getcup(string cupname)
+    void fill()
     {
+        script.getcupandsetvol(Name, volume);
+        thisoptionscanvas.SetActive(false);
+    }
+
+    void empty()
+    {
+        script.getcupandsetvol(Name, 0);
+        thisoptionscanvas.SetActive(false);
+    }
+
+    void pour()
+    {
+        string othercup;
+        bool selectpour = true;
+        thisoptionscanvas.SetActive(false);
+        for (int i = 0; i < game.transform.childCount; i++)
+        {
+            if (game.transform.GetChild(i).gameObject != this.gameObject)
+            {
+                game.transform.GetChild(i).gameObject.GetComponent<cup>().enabled = false;
+            }
+        }
+        while (selectpour)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    for (int i = 0; i < game.transform.childCount; i++)
+                    {
+                        if (hit.transform.gameObject.name == game.transform.GetChild(i).gameObject.name)
+                        {
+                            othercup = game.transform.GetChild(i).gameObject.name;
+                            selectpour = false;
+                            script.pour(othercup, currentvol);
+                            break;
+                        }
+
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < game.transform.childCount; i++)
+        {
+            if (game.transform.GetChild(i).gameObject != this.gameObject)
+            {
+                game.transform.GetChild(i).gameObject.GetComponent<cup>().enabled = true;
+            }
+        }
+
+
+
+
+
         
     }
 }
-
 
---
+++ cupcreate.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cupcreate : MonoBehaviour
+{
+
+
+    public List<string> cupsname;
+    public List<int> cupsvol;
+    public List<cupcreate> cupslist;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < cupsname.Count; i++){
+            //need to create new var for each cup
+            cupcreate test  = new cupcreate(cupsname[i], cupsvol[i], 0);
+            cupslist.Add(test);
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //test to see if list works (currently working)
+
+        if(Input.GetKeyDown(KeyCode.A)){
+            Debug.Log(cupslist[0].Name);
+            Debug.Log(cupslist[0].CurrentVolume);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            getcupandsetvol("testcup", 1);
+        }
+
+    }
+
+    public string Name { get; set; }
+    public int Volume { get; set; }
+    public int CurrentVolume { get; set; }
+    public cupcreate(string name, int vol, int curvol)
+    {
+        Name = name;
+        Volume = vol;
+        CurrentVolume = curvol;
+    }
+
+    public void getcupandsetvol(string c, int i)
+    {
+        
+        foreach(cupcreate t in cupslist){
+            if(t.Name == c){
+                
+                t.CurrentVolume = i;
+            }
+        }
+    }
+
+    public void pour(string othercup, int i){
+        
+        foreach (cupcreate t in cupslist)
+        {
+            if (t.Name == othercup)
+            {
+                if (i + t.CurrentVolume <= t.Volume)
+                {
+                    t.CurrentVolume = i + t.CurrentVolume;
+                }
+                else{
+                    t.CurrentVolume = t.Volume;
+                }
+            }
+        }
+             
+            
+
+    }
+}
+
+
