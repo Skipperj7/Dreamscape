ac7e8c4e3decced412d3a255f7bf7a029b4f3be1
Jacob Cunningham <jacob.joc@gmail.com>
2019-01-28 11:35:27 -0800

    Add files via upload

---
+++ clickonmissing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickonmissing : MonoBehaviour
+{
+
+
+    public int spritenumber;
+    public int setamount;
+    public string levelname;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spritenumber == setamount){
+            switch(levelname){
+                case "level 1 spot the difference 1":
+                    
+                    break;
+            }
+        }
+
+
+    }
+}
---
+++ objectmissing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectmissing : MonoBehaviour {
+
+    private Button btn;
+    public clickonmissing s;
+	// Use this for initialization
+	void Start () {
+        btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(clicked);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void clicked(){
+        s.spritenumber++;
+        Destroy(gameObject);
+    }
+
+}
