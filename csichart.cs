553b3b2794ebbe9708f5e30c6459bb85ebdfcaf2
Jacob Cunningham <jacob.joc@gmail.com>
2019-02-01 12:11:32 -0800

    Add files via upload

---
+++ csichart.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class csichart : MonoBehaviour
+{
+
+    public List<SpriteRenderer> boxes;
+    public List<string> answers;
+    public List<string> correctanswers;
+    public int numcorrect;
+    public int totalnum;
+
+    public Sprite check;
+    public Sprite blank;
+    public Sprite x;
+
+    public Button submitbtn;
+    public Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        submitbtn.onClick.AddListener(checkanswers);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void checkanswers() { 
+        foreach(SpriteRenderer s in boxes) {
+            if (s.sprite == check)
+            {
+                answers.Add("check");
+            }
+            else if (s.sprite == x)
+            {
+                answers.Add("x");
+            }
+            else if (s.sprite == blank)
+            {
+                answers.Add("blank");
+            }
+            
+        }
+
+        for(int i = 0; i < totalnum;i++) {
+            Debug.Log(i);
+            if(answers[i] == correctanswers[i]) { 
+            numcorrect++;
+            }
+        }
+
+        float percent = (100 * numcorrect) / (totalnum);
+        score.text = percent + "%";
+    }
+
+}
---
+++ csichartbutton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csichartbutton : MonoBehaviour
+{
+    public SpriteRenderer btn;
+    public Sprite check;
+    public Sprite blank;
+    public Sprite x;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            if (btn.sprite == check)
+            {
+                btn.sprite = blank;
+            }
+            else
+            {
+                btn.sprite = check;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (btn.sprite == x)
+            {
+                btn.sprite = blank;
+            }
+            else
+            {
+                btn.sprite = x;
+            }
+        }
+    }
+
+
+}
